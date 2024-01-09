e499f0075e5bc7eb1ddb4bdf17c46d78a932f7b6
Steven <65731775+szhao23@users.noreply.github.com>
2024-01-09 14:29:48 -0600

    Removed Fake Dummy Data, added Data from API and modified src image URL because was incompatible

--- code/NextJS-InfiniteScroll-Anime-App/app/action.ts
+++ code/NextJS-InfiniteScroll-Anime-App/app/action.ts
@@ -1,13 +1,15 @@
 "use server"; // Whatever written in this code will be executed as a server action
 
-export const fetchAnime = async () => {
+export const fetchAnime = async (page: number) => {
   // Fetch the Data from URL
-  const response = await fetch("https://shikimori.one/api/animes");
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+  );
 
   // Set Response Data into Variable
   const data = await response.json();
 
-  console.log("Response: ", data);
+  //   console.log("Response: ", data);
 
   // Return Data after Response has been Fetched
   return data;
